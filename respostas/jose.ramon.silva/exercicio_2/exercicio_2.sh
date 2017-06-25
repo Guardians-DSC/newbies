1b9d05ba8e18560b530d86916253f539b1492125
ramon <jose.ramon.silva@ccc.ufcg.edu.br>
2017-06-25 13:33:12 -0300

    Exercicio 3 finalizado até a metade e ajustes nos comments.

--- respostas/jose.ramon.silva/exercicio_2/exercicio_2.sh
+++ respostas/jose.ramon.silva/exercicio_2/exercicio_2.sh
@@ -1,40 +1,59 @@
 #!/bin/bash
+#
 # José Ramon Fragoso da Silva - 116210412
+#
+# jose.ramon.silva@ccc.ufcg.edu.br
+#
 # exercicio_2 -  Script exemplo de reposta para as questoes dos guardians.
+#
 ###############################################################################################################
 
 read N # Le a quantidade de vezes que vai repetir
 read S # Le o tempo
 read P_USER	# Le o usuario
 
-for i in `seq 1 $N`	# Repete N vezes
+# Repete o loop N vezes
+for i in `seq 1 $N`	
 do
-	sleep "$S" # Delay  de S segundos para executar os proximos comandos
-	ps -ef | grep "$P_USER" >> processos.txt # Lista todos os processos que contem a string P_USER e armazena em um arquivo processos.txt
+	# Delay  de S segundos para executar os proximos comandos
+	sleep "$S" 
+	# Lista todos os processos que contem a string P_USER e armazena em um arquivo processos.txt
+	ps -ef | grep "$P_USER" >> processos.txt 
 
 done
 
 # %CPU = Coluna 3 | %MEM = Coluna 4
 
-cat processos.txt | awk '{print $3}' > cpu_processos.txt # Lista todos os processos de CPU em processos.txt e armazena em outro arquivo
-cat processos.txt | awk '{print $4}' > mem_processos.txt # Lista todos os processos de MEM em processos.txt e armazena em outro arquivo
+# Lista todos os processos de CPU em processos.txt e armazena em outro arquivo
+cat processos.txt | awk '{print $3}' > cpu_processos.txt 
 
-maiorcpu=0 # Variavel para ser armazenado o maior %CPU
+# Lista todos os processos de MEM em processos.txt e armazena em outro arquivo
+cat processos.txt | awk '{print $4}' > mem_processos.txt 
+
+
+# Variavel para ser armazenado o maior %CPU
+maiorcpu=0 
+
+# Variavel para ser armazenado o menor %CPU
 menorcpu=`sed -n "1p" cpu_processos.txt ` # Variavel para ser armazenado o menor %CPU
 
-while read linha # Loop que ir ler cada linha do arquivo cpu_processos.txt 
+# Loop que ir ler cada linha do arquivo cpu_processos.txt 
+while read linha
 do
-	if [[ $linha -gt $maiorcpu ]]; then	# Parte responsavel por armazenar os maiores valores encontrados na variavel maiorcpu
+	# Parte responsavel por armazenar os maiores valores encontrados na variavel maiorcpu
+	if [[ $linha -gt $maiorcpu ]]; then	
 	 	maiorcpu=$linha;
 	 fi 
 
-	 if [[ $linha -lt $menorcpu ]]; then # Parte responsavel por armazenar os menores valores encontrados na variavel menorcpu
+	# Parte responsavel por armazenar os menores valores encontrados na variavel menorcpu
+	 if [[ $linha -lt $menorcpu ]]; then 
 	 	menorcpu=$linha
 	 fi
 
 done < cpu_processos.txt
 
-maiormem=0 # Repeticao de tudo o que foi feito com %CPU, mas agora utilizando o %MEM
+# Repeticao do que foi feito com CPU, mas agora feito com MEM.
+maiormem=0 
 menormem=`sed -n "1p" mem_processos.txt `
 
 while read linha
