aea20157e03d82e0d2bdb65d3aba721a8241a004
David Ferreira Quaresma <david.quaresma@ccc.ufcg.edu.br>
2017-06-29 22:27:47 -0300

    exercicios 1 e 2 concluidos

--- respostas/david.quaresma/exercicio_1/resposta.sh
+++ respostas/david.quaresma/exercicio_1/resposta.sh
@@ -4,29 +4,33 @@
 
 # Copio o arquivo .txt.gz em /questoes para a pasta local, como Victor sugeriu.
 cp ../../../questoes/exercicio_1/jan_95.txt.gz ./
+
 # Descompacto o arquivo com o comando gunzip -k, onde o -k evita que o gunzip apague o .gz inicial.
 gunzip -k jan_95.txt.gz
 
-# Salvo as linhas com os padroes desejados em outro arquivo.
+# salvo as linhas com os padrões desejados em outro arquivo.
 sed '/ - - /!d'  jan_95.txt > jan_valido.txt
 
+# crio contadores.
 remotecount=0
 localcount=0
+
 #leio o novo arquivo linha por linha.
 while read line; do
 
-    # verifico se a linha contem o padrao de interesse.
+    # verifico se a linha contém o padrao de interesse.
     if echo "$line" | egrep 'remote' >/dev/null; then
-	# incremento 1 na variavel que contabiliza as repeticoes desse padrao. 
+
+	# incremento 1 na variável que contabiliza as repetições desse padrão. 
 	remotecount=$((remotecount+1))
-        
+    
     elif echo "$line" | egrep 'local' >/dev/null; then 
 	localcount=$((localcount+1))
     fi
 
 done < jan_valido.txt
 
-# exibo os resultados 
+# exibo os resultados. 
 echo $remotecount 'requisições remotas realizadas'
 echo $localcount 'requisições locais realizadas'
 
