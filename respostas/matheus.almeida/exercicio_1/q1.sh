93770181395652711317c5d3b85cf4146c8ed06c
MatheusCMelo <matheus.almeida@ccc.ufcg.edu.br>
2017-07-01 12:44:05 -0300

    exercicio 1 corrigido

--- respostas/matheus.almeida/exercicio_1/q1.sh
+++ respostas/matheus.almeida/exercicio_1/q1.sh
@@ -5,7 +5,7 @@
 cp ../../../questoes/exercicio_1/jan_95.txt.gz ./
 gzip -d jan_95.txt.gz
 
-cat jan_95.txt | uniq - - > valid_lines.txt
+cat jan_95.txt | grep "\- -" > valid_lines.txt
 
 local_requests=0
 remote_requests=0
