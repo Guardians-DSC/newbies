cd1aea6ac5af2edac8437b526b7b06fe6713e09d
lucasmbarreto <lucasbarretogk@gmail.com>
2017-07-01 23:09:50 -0300

    Adicionando a pasta matricula

---
+++ respostas/lucas.barreto/Exercicio_1/questao1.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Lucas Matheus Ferreira Barreto - 116210932
+# Exercicio 1 - Script de resposta para seleção dos Guardians.
+
+
+# Descompacta o arquivo jan_95.txt.gz
+gunzip jan_95.txt.gz
+
+# Gera um arquivo remote.txt apenas com os remotes válidos
+cat jan_95.txt | grep "remote - -" > remote.txt
+
+# Gera um arquivo local.txt apenas com os locales válidos
+cat jan_95.txt | grep "local - -" > local.txt
+
+# Atribuo a quantidade de comandos de cada tipo de solicitação às respectivas variáveis
+removidas=`cat remote.txt | wc -l`
+locais=`cat local.txt | wc -l`
+
+# Imprime as variáveis correspondentes às solicitações remotas e locais
+echo $removidas
+echo $locais
+
