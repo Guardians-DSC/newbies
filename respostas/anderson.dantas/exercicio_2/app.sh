c15344ba05013bb0448dba91d1abdf8714bbb614
Anderson Kleber <a.kleber.d@gmail.com>
2017-07-02 21:33:50 -0300

    Corrigido alguns problemas no Ex2

--- respostas/anderson.dantas/exercicio_2/app.sh
+++ respostas/anderson.dantas/exercicio_2/app.sh
@@ -12,48 +12,54 @@ menor_mem=100
 
 user_found=0
 
-while read -r line
-
+t=0
+while ((t++ < $N))
 do
+  while read -r line
 
-  line=($line)
-  if [[ ${line[0]} =~ $P_USER ]]
+  do
+
+    line=($line)
+    if [[ ${line[0]} =~ $P_USER ]]
+    then
+      user_found=1
+
+      cpu=${line[2]}
+      mem=${line[3]}
+
+      if [[ $(echo "$cpu > $maior_cpu" | bc) -eq 1 ]];
+      then
+        maior_cpu=$cpu
+      fi
+
+      if [[ $(echo "$cpu < $menor_cpu" | bc) -eq 1 ]];
+      then
+        menor_cpu=$cpu
+      fi
+
+      if [[ $(echo "$mem > $maior_mem" | bc) -eq 1 ]];
+      then
+        maior_mem=$mem
+      fi
+
+      if [[ $(echo "$mem < $menor_mem" | bc) -eq 1 ]];
+      then
+        menor_mem=$mem
+      fi
+    fi
+  done < <(ps -aux | sed -n '1!p')
+
+
+  if [[ $user_found -eq 1 ]]
   then
-    user_found=1
-
-    cpu=${line[2]}
-    mem=${line[3]}
-
-    if [[ $(echo "$cpu > $maior_cpu" | bc) -eq 1 ]];
-    then
-      maior_cpu=$cpu
-    fi
-
-    if [[ $(echo "$cpu < $menor_cpu" | bc) -eq 1 ]];
-    then
-      menor_cpu=$cpu
-    fi
-
-    if [[ $(echo "$mem > $maior_mem" | bc) -eq 1 ]];
-    then
-      maior_mem=$mem
-    fi
-
-    if [[ $(echo "$mem < $menor_mem" | bc) -eq 1 ]];
-    then
-      menor_mem=$mem
-    fi
+    echo 'CPU - menor: '$menor_cpu' | maior: '$maior_cpu
+    echo 'Memória - menor: '$menor_mem' | maior: '$maior_mem
+    # echo $menor_mem $menor_cpu $maior_mem $maior_cpu
+  else
+    echo "Não existe nenhum processo para usuários iniciados em '$P_USER'"
   fi
-done < <(ps -aux | sed -n '1!p')
 
+  echo '----------------------------------------------'
+  sleep $S
 
-if [[ $user_found -eq 1 ]]
-then
-  echo 'CPU - menor: '$menor_cpu' | maior: '$maior_cpu
-  echo 'Memória - menor: '$menor_mem' | maior: '$maior_mem
-  # echo $menor_mem $menor_cpu $maior_mem $maior_cpu
-else
-  echo "Não existe nenhum processo para usuários iniciados em '$P_USER'"
-fi
-
-sleep $S
+done
