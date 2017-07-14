aeb23b7febd652e32369f600230944460202f3ce
Victor Hugo <victorhundo@gmail.com>
2017-07-14 08:56:46 -0300

    Adicionado novamente o exercicio exemplo removido no merge

---
+++ respostas/victor.sousa/exercicio_1/resposta.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Victor Hugo Fernandes de Sousa - 113210154
+# exercicio_1 - Script exemplo de reposta para as questoes dos guardians
+
+# Copio o arquivo .txt.gz em /questoes para a pasta local e depois descompacto
+cp ../../../questoes/exercicio_1/jan_95.txt.gz ./
+tar -vzxf jan_95.txt.gz
+
+# Só sei fazer até aqui
