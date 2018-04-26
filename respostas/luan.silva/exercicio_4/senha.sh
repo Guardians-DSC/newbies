3121af07174d43415d477733d553e1c6f5d82a78
Luan Rocha <luan.silva@ccc.ufcg.edu.br>
2018-04-26 10:58:54 -0300

    quest 4 concluida

--- respostas/luan.silva/exercicio_4/senha.sh
+++ respostas/luan.silva/exercicio_4/senha.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-echo "Entre com a senha: " 
-read senha
+read -s -p "Entre com a senha: " senha
+echo
 
 comp=$(expr length $senha)
+regex1=[0-9]+
+regex2=[A-Z]+
 
 if [ $comp -lt 8 ]
 	then
-		echo "Senha Inválida."
-		echo "Por segurança não são aceitas senhas com menos de
-		6 caracteres ou mais que 9."
-		echo "Informe uma nova senha."
-	else
-		echo "Senha segura!"
+		echo "Senha insegura! Deve conter pelo menos 8 caracteres."
+elif ! [[ $senha =~ $regex1 ]]
+	then
+		echo "Senha insegura! Deve conter letras e números."
+elif ! [[ $senha =~ $regex2 ]]
+	then
+		echo "Senha insegura! Deve conter pelo menos uma letra maiúscula."
+else
+	echo "Senha segura! :)"
 fi
 
 
