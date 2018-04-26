4076c707d2b83ad9e58f8b849c9b97032d7f2722
davidedup <david.pereira@ccc.ufcg.edu.br>
2018-04-26 15:32:27 -0300

    questão4 done

---
+++ respostas/116110322/questao4.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Digite sua senha:"
+
+read senha
+
+if echo "${senha}" | grep '[0-9]' >/dev/null; then
+	if [[ $senha =~ [A-Z] ]]; then
+		if [[ $senha =~ [A-Z] ]]; then
+			echo "Senha Forte ;D"
+		else
+			echo "Senha Fraca ;("		
+		fi
+	else
+		echo "Senha Fraca ;("	
+	fi	
+else
+	echo "Senha Fraca ;("
+fi
+
+
