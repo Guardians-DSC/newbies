de7ee5e7469c43205c8f4236b7fbd9b1d35ae113
arthurferrao <arthurdeferrao@gmail.com>
2018-04-30 21:03:00 -0300

    correção exercicio_5

--- respostas/arthur.ferrao/exercicio_5.sh
+++ respostas/arthur.ferrao/exercicio_5.sh
@@ -11,20 +11,16 @@ cont=0
 for line in $(lsof | grep UDP | cut -d' ' -f1 | sort) ;
    do 
       atual="$line"
-
       if [ "$anterior" != "$atual" ];
          then
             cont=0
       fi
-
       cont=$(($cont+1))
-
       if [ $cont -gt $maior ];
          then
             maior=$cont
             comando="$line"
       fi 
-
       anterior="$line"
    done 
 
@@ -40,20 +36,16 @@ cont=0
 for line in $(lsof | grep TCP | cut -d' ' -f1 | sort) ;
    do 
       atual="$line"
-
       if [ "$anterior" != "$atual" ];
          then
             cont=0
       fi
-
       cont=$(($cont+1))
-   
       if [ $cont -gt $maior ];
          then
             maior=$cont
             comando="$line"
       fi 
-
       anterior="$line"
    done 
 
