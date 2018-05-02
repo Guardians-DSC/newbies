efc41cae45d56a26206d7e503cf38120c10b294e
Eduardo Pontes <eduardo.henrique.silva@ccc.ufcg.edu.br>
2018-05-02 20:57:07 -0300

    Modificação da Questão 6

--- respostas/eduardo.henrique.silva/exercicio_6.sh
+++ respostas/eduardo.henrique.silva/exercicio_6.sh
@@ -4,11 +4,11 @@
 # Login_CCC: eduardo.henrique.silva
 # Script: Exercício 6
 
-teste=$(ls -l | cut -c7-9 | grep -E -- "-w-|rw-|-wx|rwx")
+teste=$(ls -l | cut -c8-10 | egrep -- "-w-|rw-|-wx|rwx" | wc -l)
 
-if [ ${#teste} -eq 0 ]; 
+if [ $teste -gt 0 ]; 
 then
-        echo "DIRETORIO SEGURO"
-else
         echo "DIRETORIO INSEGURO"
+else
+        echo "DIRETORIO SEGURO"
 fi
