ad0132041e93f2ff2688defa11e75b3331b767a5
Ramon Sousa Sarmento <ramon.sarmento@ccc.ufcg.edu.br>
2018-10-31 09:12:39 -0300

    Hacktober PR

--- respostas/ramon.sarmento/Questão 6/Q6.sh
+++ respostas/ramon.sarmento/Questão 6/Q6.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # Ramon Sousa Sarmento,117210402
 
-if [ `ls -l | awk '{if(substr($1,9,1) == "w"){print 0;exit 0}}'` ];then echo "diretorio não seguro";else echo "diretorio seguro";fi
+if [ `ls -l | awk '{if(substr($1,9,1) == "w"){print 0;exit 0}}'` ];then
+       	echo "diretorio não seguro";
+else echo "diretorio seguro";
+fi
