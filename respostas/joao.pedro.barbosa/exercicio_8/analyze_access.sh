82dc975d9e758434735563cec89fc22d24dd6295
joaobb <joao.pedro.barbosa@ccc.ufcg.edu.br>
2019-04-05 20:56:24 -0300

    Ultimo

--- respostas/joao.pedro.barbosa/exercicio_8/analyze_access.sh
+++ respostas/joao.pedro.barbosa/exercicio_8/analyze_access.sh
@@ -10,6 +10,6 @@ year=$1
 echo -ne "Quantidade de arquivos impressos em $year: "
 cat access.log | grep $year | grep -c "Send-Document"
 echo -ne "Quantidade de bytes transimitidos em $year: "
-cat access.log | grep $year | awk '{ SUM += $2 } END {print SUM}'
+cat access.log | grep $year | awk '{ SUM += $2 } END {print SUM " bytes"}'
 
 rm *.log
